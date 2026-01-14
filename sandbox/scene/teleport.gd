197c056aa9b641be98301bdf05fc8778c193b1fe
Alessandro404 <a.chachlakis@gmail.com>
2026-01-14 23:07:17 +0100

    Finalizzato lavoro per corso

--- sandbox/scene/teleport.gd
+++ sandbox/scene/teleport.gd
@@ -8,18 +8,31 @@ class_name Teleport
 
 var cooldown : bool = false
 
+enum Levels {LIVELLO1, LIVELLO2, LIVELLO3, LIVELLO4, LIVELLO5}
+@export var livello_obiettivo: Levels
 
 func _ready() -> void:
 	Singleton.add_to_teleport(self)
 
 
 func send():
+	var level_name_parts =  get_tree().get_current_scene().get_name().split("_")
+
+	if level_name_parts.size() > 1:
+
+		if not level_name_parts[1].to_int() == livello_obiettivo+1:
+		
+			SceneTransition.load_and_change_level(livello_obiettivo, destinazione)
+	else: SceneTransition.load_and_change_level(livello_obiettivo, destinazione)
+	
 	for node in Singleton.registered_teleports:
 		if node.nome_teletrasporto == destinazione:
 			node.receive()
 
-func receive():
-	SceneTransition.fake_dissolve()
+func receive(dissolve = true):
+	if dissolve:
+		SceneTransition.fake_dissolve()
+		await get_tree().create_timer(0.5).timeout
 	cooldown = true
 	player.global_position = global_position
 	
@@ -27,11 +40,14 @@ func receive():
 
 func _on_body_entered(body: Node3D) -> void:
 	if body == player and not cooldown:
-		#print("entrato player")
 		send()
 
 
 func _on_body_exited(body: Node3D) -> void:
 	if body == player:
-		print("Uscito player")
 		cooldown = false
+
+func _exit_tree() -> void:
+	# Remove this teleport from the list when the level changes
+	if Singleton.registered_teleports.has(self):
+		Singleton.registered_teleports.erase(self)
